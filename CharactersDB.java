3b2be8a1d4d8b5267929cb236e38b25d05f9ef32
LuiggiFdz <a01023675@itesm.mx>
2018-05-24 18:27:00 -0500

    prog

---
+++ CharactersDB.java
@@ -0,0 +1,64 @@
+package DB;
+
+import java.io.IOException;
+
+/**
+ * @author LuiggiFernandez
+ */
+public class CharactersDB {
+    
+    static String FirstName;
+    static String LastName;
+    static String Gender;
+    static int Age;
+    static String Role;
+    static boolean hasJob;
+    
+    public static int Random(int Options) {
+        int Random = (int)(Math.random()*Options);
+        return Random;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 100; i++) {
+            DBInternal Method = new  DBInternal();
+            Method.DBInternal(FirstName(), LastName(), Gender(), Age(), Role(), hasJob());
+        }
+    }    
+    
+    public static String FirstName() {
+        String FirstNames[] = {"Alex", "Andy", "Blake", "Brook", "Campbell", "Chris", "Delta", "Dorian", "Eli", "Emery", "Finley", "Frances", "Gabriel", "Glenn", "Harley", "Hunter", "Jean",  "Jesse", "Kaden", "Kai", "Lake", "Lou", "Mason", "Max", "Pat", "Phoenix", "Ray", "Reese", "Sage", "Sydney", "Tanner", "Tatum", "West", "Winter"};
+        FirstName  = FirstNames[Random(34)];
+        return FirstName;
+    }
+    public static String LastName() {
+        String LastNames[] = {"Abernathy", "Babbit", "Caffee", "Dabkowski", "Enoch", "Fernandez", "Goude", "Hutch", "Ivanoff", "Jeralds", "Kwong", "Lablanc", "Myrtle", "Najar", "Ozuna", "Pagani", "Quirk", "Rosberg", "Szymkowiak", "Taft", "Umbridge", "Verner", "Wei", "Xin", "Yi", "Zak"};
+        LastName  = LastNames[Random(25)];
+        return LastName;
+    }
+    
+    public static String Gender() {
+        String Genders[] = {"Male",  "Female"};
+        Gender = Genders[Random(2)];
+        return Gender;
+    }
+    
+    public static int Age() {
+        int Ages[] = {18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75};
+        Age = Ages[Random(58)];
+        return Age;
+    }
+    
+    public static String Role() {
+        String Roles[] = {"Protagonist", "Antagonist", "Mentor", "Tempter",  "Sidekick", "Skeptic", "Emotional", "Logical", "Extra"};
+        Role = Roles[Random(9)];
+        return Role;
+    }
+    
+    public static boolean hasJob() {
+        boolean hasJobs[] = {true, false};
+        hasJob = hasJobs[Random(2)];
+        return hasJob;
+    }
+    
+}
---
+++ DBInternal.java
@@ -0,0 +1,50 @@
+package DB;
+import java.io.*;
+/**
+ * @author LuiggiFernandez
+ */
+public class DBInternal {
+    
+    private String FirstName;
+    private String LastName;
+    private String Gender;
+    private String Age;
+    private String Role;
+    private String hasJob;
+    
+    
+    
+    public void DBInternal(String FirstName, String LastName, String Gender,  int Age, String Role, boolean hasJob) throws IOException {
+        File CharactersDB = new File("/Users/LuiggiFernandez/Desktop/Progs/Luiggi_Money/txtDataBase/Characters.txt");
+        try (FileReader FileReaderCharactersDB = new FileReader(CharactersDB); BufferedReader BufferedReaderChacractersDB = new BufferedReader(FileReaderCharactersDB)) {
+            try (FileWriter FileWriterCharactersDB = new FileWriter(CharactersDB, true); BufferedWriter BufferedWriterCharactersDB = new BufferedWriter(FileWriterCharactersDB)) {
+                String Line;
+                String LineColumn[];
+                this.Age = Integer.toString(Age);
+                if (hasJob) {
+                    this.hasJob = "Employed";
+                } else {
+                    this.hasJob = "Unemployed";
+                }   
+                Line = BufferedReaderChacractersDB.readLine();
+                while (Line != null) Line = BufferedReaderChacractersDB.readLine();
+                if (Line == null) {
+                    BufferedWriterCharactersDB.write(Format(FirstName, 9)+"| "+Format(LastName, 12)+"| "+Format(Gender, 7)+"| "+Format(this.Age, 4)+"| "+Format(Role, 13)+"| "+this.hasJob+"\n");
+                }
+                BufferedWriterCharactersDB.close();
+                FileWriterCharactersDB.close();
+            }
+            BufferedReaderChacractersDB.close();
+            FileReaderCharactersDB.close();
+        }
+        System.out.print(Format(FirstName, 9)+"| "+Format(LastName, 12)+"| "+Format(Gender, 7)+"| "+Format(this.Age, 4)+"| "+Format(Role, 13)+"| "+this.hasJob+"\n");
+    }
+    
+    public static String Format(String WORD, int SIZE){
+        while(WORD.length() < SIZE){
+            WORD += " "; 
+        }
+        return WORD;
+    }
+    
+}
